f4b6169ba2c0a804ceea7fc49c7ba80c382378b9
Leandro Reis <lleandrofr@gmail.com>
2021-06-15 11:12:18 -0300

    install crypto-js and render player profile picture on header componenet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 
 class Header extends Component {
   render() {
     const { name, gravatarEmail } = this.props;
+    const hash = md5(gravatarEmail).toString;
     return (
       <header>
-        <img src={ `https://www.gravatar.com/avatar/${hash}` } alt="Avatar do jogador" />
+        <img
+          src={ `https://www.gravatar.com/avatar/${hash}` }
+          alt="Avatar do jogador"
+          data-testid="header-profile-picture"
+        />
         <p data-testid="header-player-name">{name}</p>
       </header>
     );
