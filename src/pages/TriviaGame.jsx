b2c29d3c189c5f040a34ff28065476f83a154f80
Leandro Reis <lleandrofr@gmail.com>
2021-06-15 14:50:20 -0300

    removes redirect from TriviaGame component

--- src/pages/TriviaGame.jsx
+++ src/pages/TriviaGame.jsx
@@ -1,21 +1,8 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router';
 import Header from '../components/Header';
 
 class TriviaGame extends Component {
-  constructor() {
-    super();
-    this.state = {
-      redirectSettings: false,
-    };
-  }
-
   render() {
-    const { redirectSettings } = this.state;
-    if (redirectSettings) {
-      return <Redirect to="settings" />;
-    }
-
     return (
       <Header />
     );
