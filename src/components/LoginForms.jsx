357053146fa36ab6abfbc840e761f5c0a53783db
Jonathan Souza <souzajonathan570@gmail.com>
2021-06-17 15:36:57 -0300

    Feito uma implementacao na validacao dos inputs de logins

--- src/components/LoginForms.jsx
+++ src/components/LoginForms.jsx
@@ -12,8 +12,12 @@ class Login extends Component {
       gravatarEmail: '',
       btnEnable: true,
       login: false,
+      errors: {},
     };
+
     this.verifyLogin = this.verifyLogin.bind(this);
+    this.nameInfo = this.nameInfo.bind(this);
+    this.emailInfo = this.emailInfo.bind(this);
   }
 
   componentWillUnmount() {
@@ -22,15 +26,74 @@ class Login extends Component {
     savePlayerInfo({ name, gravatarEmail });
   }
 
+  // verifyLogin() {
+  //   const { name, gravatarEmail } = this.state;
+  //   if (name.length > 0 && gravatarEmail.length > 0) {
+  //     this.setState({ btnEnable: false });
+  //   }
+  // }
   verifyLogin() {
     const { name, gravatarEmail } = this.state;
+    const errors = {};
     if (name.length > 0 && gravatarEmail.length > 0) {
       this.setState({ btnEnable: false });
     }
+
+    if (name.length === 0) {
+      errors.errorName = 'Por favor, insira um nome';
+      this.setState({ btnEnable: true, errors });
+    }
+
+    if (!/(\w+[0-9]*)+@\w+\.\w+/.test(gravatarEmail)) {
+      errors.errorEmail = 'Por favor, insira um email válido';
+      this.setState({ btnEnable: true, errors });
+    }
+
+    return this.setState({ errors });
   }
 
   redirectToGame() {
     this.setState({ login: true });
+  }
+
+  nameInfo() {
+    const { name, errors } = this.state;
+    return (
+      <label htmlFor="name">
+        Nome:
+        <input
+          data-testid="input-player-name"
+          id="name"
+          name="name"
+          type="text"
+          value={ name }
+          onChange={ ({ target }) => {
+            this.setState({ name: target.value }, this.verifyLogin);
+          } }
+        />
+        {errors.errorName && <span>{errors.errorName}</span>}
+      </label>
+    );
+  }
+
+  emailInfo() {
+    const { gravatarEmail, errors } = this.state;
+    return (
+      <label htmlFor="email">
+        Email:
+        <input
+          data-testid="input-gravatar-email"
+          id="email"
+          name="email"
+          type="text"
+          value={ gravatarEmail }
+          onChange={ ({ target }) => {
+            this.setState({ gravatarEmail: target.value }, this.verifyLogin);
+          } }
+        />
+        {errors.errorEmail && <span>{errors.errorEmail}</span>}
+      </label>
+    );
   }
 
   render() {
@@ -42,30 +105,8 @@ class Login extends Component {
     return (
       <section>
         <form>
-          <label htmlFor="name">
-            Nome:
-            <input
-              type="text"
-              id="name"
-              name="name"
-              data-testid="input-player-name"
-              onChange={ ({ target }) => {
-                this.setState({ name: target.value }, this.verifyLogin);
-              } }
-            />
-          </label>
-          <label htmlFor="email">
-            Email:
-            <input
-              type="text"
-              id="email"
-              name="email"
-              data-testid="input-gravatar-email"
-              onChange={ ({ target }) => {
-                this.setState({ gravatarEmail: target.value }, this.verifyLogin);
-              } }
-            />
-          </label>
+          { this.nameInfo() }
+          { this.emailInfo() }
           <button
             type="button"
             disabled={ btnEnable }
