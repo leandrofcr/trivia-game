953bac34799965255ab8da17616ec7091f1db664
Leandro Reis <lleandrofr@gmail.com>
2021-06-19 09:24:41 -0300

    creates a mount animation for the login container

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router';
 import { FiSettings } from 'react-icons/fi';
+import { Transition, animated, config } from 'react-spring';
 import LoginForms from '../components/LoginForms';
 import '../login.css';
 
@@ -9,28 +10,57 @@ class Login extends Component {
     super();
     this.state = {
       redirectSettings: false,
+      showComp: false,
     };
   }
 
+  componentDidMount() {
+    this.showComp();
+  }
+
+  componentWillUnmount() {
+    this.showComp();
+  }
+
+  showComp() {
+    const { showComp } = this.state;
+    this.setState({ showComp: !showComp });
+  }
+
   render() {
-    const { redirectSettings } = this.state;
+    const { redirectSettings, showComp } = this.state;
     if (redirectSettings) {
       return <Redirect to="/settings" />;
     }
     return (
       <section className="login">
-        <section className="login-container">
-          <LoginForms />
-          <button
-            type="button"
-            data-testid="btn-settings"
-            className="settings-btn"
-            onClick={ () => this.setState({ redirectSettings: true }) }
-          >
 
-            <FiSettings />
-          </button>
-        </section>
+        <Transition
+          items={ showComp }
+          from={ { x: 500, opacity: 0, scale: 0.5 } }
+          enter={ { x: 0, opacity: 1, scale: 1 } }
+          leave={ { x: -500, opacity: 0, scale: 0.5 } }
+          delay={ 200 }
+          config={ config.molasses }
+        >
+          {(styles, item) => item
+          && (
+            <animated.div style={ styles }>
+              <section className="login-container">
+                <LoginForms />
+                <button
+                  type="button"
+                  data-testid="btn-settings"
+                  className="settings-btn"
+                  onClick={ () => this.setState({ redirectSettings: true }) }
+                >
+
+                  <FiSettings />
+                </button>
+              </section>
+            </animated.div>)}
+        </Transition>
+
       </section>
 
     );
