a4ab6ffbe89ca6c9e92f1d4c26c58ef1911e7d30
Leandro Reis <lleandrofr@gmail.com>
2021-09-25 14:28:48 -0300

    Updates

---
+++ precache-manifest.cefa3d433ffbe6dd8afc291ff5689760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520b5c93cc39be26e85e8f3c4f2cbead",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "9ff9eed97211205bc983",
+    "url": "/trivia-game/static/css/main.ca431c48.chunk.css"
+  },
+  {
+    "revision": "760b16963fa105120f87",
+    "url": "/trivia-game/static/js/2.ca0aaeb9.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trivia-game/static/js/2.ca0aaeb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ff9eed97211205bc983",
+    "url": "/trivia-game/static/js/main.91abfcb9.chunk.js"
+  },
+  {
+    "revision": "28c450d90070e1cbeaea",
+    "url": "/trivia-game/static/js/runtime-main.34810379.js"
+  }
+]);
